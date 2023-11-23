a07de725afe195529b5f1c12b6b8e50a45c6d533
Adrian Chwiłkowski <adrian.chwilkowski@i4b.pl>
2023-11-23 14:55:55 +0100

    Add project files.

---
+++ Infrastructure/Entities/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Entities
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public double FullCost { get; set; }
+        public int DeliveryType { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
---
+++ Tests/Samples/GenerateOrders.cs
@@ -0,0 +1,84 @@
+﻿using Infrastructure.Entities;
+using Infrastructure.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Samples
+{
+    public class GenerateOrders
+    {
+        private List<OrderedBooks> _orderedBooks;
+        private List<BookInfo> _bookInfo;
+        public GenerateOrders(List<BookInfo> bookInfo, List<OrderedBooks> orderedBooks)
+        {
+            _bookInfo = bookInfo;
+            _orderedBooks = orderedBooks;
+        }
+        public List<Order> Generate()
+        {
+            List<Order> orders = new List<Order>();
+            double price = 0;
+            var userId = Guid.NewGuid();
+            foreach(var orderedBook in _orderedBooks)
+            {
+                try
+                {
+                    price = GetBookPriceByBookId(_bookInfo, orderedBook.Id);
+                    var order = orders.First(o => o.Id == orderedBook.OrderId);
+                }
+                catch(NotFoundException ex)
+                {
+                    throw ex;
+                }
+                catch(Exception)
+                {
+                    CreateOrder(orders, price, orderedBook.Amount, 1, userId);
+                }
+            }
+            return orders;
+        }
+        private void CreateOrder(List<Order> orders, double price , int amount, int deliveryType, Guid userId)
+        {
+            var order = new Order
+            {
+                Id = Guid.NewGuid(),
+                FullCost = price * amount,
+                DeliveryType = deliveryType,
+                UserId = userId
+            };
+            orders.Add(order);
+        }
+        private void AddToOrder(Order order, double price, int amount)
+        {
+            order.FullCost += price * amount;
+        }
+        private double GetBookPriceByBookId(List<BookInfo> bookInfo, Guid bookId)
+        {
+            try
+            {
+                var result = bookInfo.First(o => o.Id == bookId).Price;
+                return result;
+            }
+            catch (Exception)
+            {
+                throw new NotFoundException("The book with given Id doesn't exist");
+            }
+        }
+        private int GetAmountOfBooksAddedToOrder(List<OrderedBooks> orderedBooks, Guid orderId)
+        {
+            try
+            {
+                var result = orderedBooks.First(o => o.Id == orderId).Amount;
+                return result;
+            }
+            catch (Exception)
+            {
+                throw new NotFoundException("The set of items with given Id doesn't exist");
+            }
+        }
+    }
+}
