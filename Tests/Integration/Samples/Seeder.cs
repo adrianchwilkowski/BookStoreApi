70f467a3bfd4f1b7daee4e76516b5cdd747ae141
Adrian Chwiłkowski <adrian.chwilkowski@i4b.pl>
2023-12-07 19:05:36 +0100

    refactored seeder

--- Tests/Integration/Samples/Seeder.cs
+++ Tests/Integration/Samples/Seeder.cs
@@ -16,6 +16,19 @@ namespace Tests.Integration.Samples
     {
         public static void Seed(ApplicationDbContext context)
         {
+            var books = SeedBooks();
+            var bookInfoList = SeedBookInfo(books);
+            var orderList = SeedOrders(10);
+            var orderItemList = SeedOrderItems(orderList, bookInfoList, 2);
+
+            context.Books.AddRange(books);
+            context.BookInfo.AddRange(bookInfoList);
+            context.Orders.AddRange(orderList);
+            context.OrderedItems.AddRange(orderItemList);
+            context.SaveChanges();
+        }
+        public static List<Book> SeedBooks()
+        {
             var bookJson = JsonConvert.DeserializeObject(File.ReadAllText(IntegrationTestBase.samplesDir + "Books.Json"));
             var bookProperties = new List<string>()
             {
@@ -35,7 +48,10 @@ namespace Tests.Integration.Samples
                     );
                 books.Add(bookToAdd);
             }
-            
+            return books;
+        }
+        public static List<BookInfo> SeedBookInfo(List<Book> books)
+        {
             var bookInfoJson = JsonConvert.DeserializeObject(File.ReadAllText(IntegrationTestBase.samplesDir + "BookInfo.Json"));
             var bookInfoProperties = new List<string>()
             {
@@ -45,15 +61,16 @@ namespace Tests.Integration.Samples
             var bookInfoList = new List<BookInfo>();
             BookInfo bookInfoToAdd;
             var i = 0;
+            var bookCount = books.Count() > bookInfoData.Count() ? bookInfoData.Count() : books.Count();
 
             double price;
             int quantity;
 
-            foreach (var bookInfo in bookInfoData)
+            foreach (var bookInfo in bookInfoData.Take(bookCount))
             {
                 price = double.Parse(bookInfo[0], CultureInfo.InvariantCulture);
                 quantity = int.Parse(bookInfo[1]);
-                
+
                 bookInfoToAdd = BookInfo.Create(
                     price,
                     quantity,
@@ -61,36 +78,35 @@ namespace Tests.Integration.Samples
                     );
                 bookInfoList.Add(bookInfoToAdd);
             }
-
+            return bookInfoList;
+        }
+        public static List<Order> SeedOrders(int orderCount)
+        {
             var orderList = new List<Order>();
-            var orderCount = 10;
-            for(var j=0; j < orderCount; j++)
+            for (var i = 0; i < orderCount; i++)
             {
                 orderList.Add(
                     Order.Create(
-                        j % 3,
+                        i % 3,
                         Guid.NewGuid()
                         ));
             }
-
-            var orderItemPerOrder = 2;
-            var orderItemCount = orderItemPerOrder * orderCount;
+            return orderList;
+        }
+        public static List<OrderedItem> SeedOrderItems(List<Order> orders, List<BookInfo> bookInfoList , int orderItemPerOrder)
+        {
+            var orderItemCount = orderItemPerOrder * orders.Count;
             var orderItemList = new List<OrderedItem>();
             Random rnd = new Random();
-            for (var j = 0; j < orderItemCount; j++)
+            for (var i = 0; i < orderItemCount; i++)
             {
                 orderItemList.Add(
                     OrderedItem.Create(
                         bookInfoList[rnd.Next(bookInfoList.Count)],
-                        orderList[j/orderItemPerOrder]
+                        orders[i / orderItemPerOrder]
                         ));
             }
-
-            context.Books.AddRange(books);
-            context.BookInfo.AddRange(bookInfoList);
-            context.Orders.AddRange(orderList);
-            context.OrderedItems.AddRange(orderItemList);
-            context.SaveChanges();
+            return orderItemList;
         }
     }
 }
