48650edbb5837510d5cf1be7706835d7bcd3d8a7
Adrian Chwiłkowski <adrian.chwilkowski@i4b.pl>
2023-12-20 18:40:04 +0100

    added tests and corrected existing

--- Tests/Integration/Tests/BooksRepositoryTests.cs
+++ Tests/Integration/Tests/BooksRepositoryTests.cs
@@ -1,4 +1,5 @@
 ﻿using Infrastructure.Entities;
+using Infrastructure.Exceptions;
 using Infrastructure.Repositories;
 using Newtonsoft.Json;
 using Tests.Integration.Samples;
@@ -8,11 +9,38 @@ namespace Tests.Integration.Tests
     public class BooksRepositoryTests : IntegrationTestBase
     {
         [Test]
-        public void CanGetBookList()
+        public async Task CanGetBookList()
         {
-            var result = booksRepository.GetBooks();
+            var result = await booksRepository.GetBooks();
 
             Assert.True(result.Any());
+        }
+        [Test]
+        public async Task CanCreateBook()
+        {
+            var booksRepository = new BooksRepository(context);
+            var book = Book.Create("title", "author", "", 128);
+
+            var books = await booksRepository.GetBooks();
+            var booksCount = books.Count;
+            await booksRepository.Create(book);
+
+            var booksAfterCreate = await booksRepository.GetBooks();
+
+            Assert.True(booksAfterCreate.Count == booksCount+1);
+        }
+        [Test]
+        public async Task CreateBook_WhileTryingAddBookWithSameId_ThrowsAlreadyExistsException()
+        {
+            var booksRepository = new BooksRepository(context);
+            var books = await booksRepository.GetBooks();
+
+            var book = Book.Create("title", "author", "", 128);
+            await booksRepository.Create(book);
+
+            Assert.ThrowsAsync<AlreadyExistsException>(async () => {
+                await booksRepository.Create(book);
+            });
         }
     }
 }
