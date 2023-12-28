5e104efa295285cb960c8fb09a5946c2897e0dd9
Adrian Chwiłkowski <adrian.chwilkowski@i4b.pl>
2023-12-28 18:26:31 +0100

    created service and added few methods

--- BookStore/Controllers/BookStoreController.cs
+++ BookStore/Controllers/BookStoreController.cs
@@ -1,12 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookStore.Services;
+using Infrastructure.Entities;
+using Infrastructure.Exceptions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookStore.Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
     public class BookStoreController : Controller
     {
-        public IActionResult Index()
+        private readonly IBookSearchService _searchService;
+        public BookStoreController(IBookSearchService searchService) { 
+            _searchService = searchService; 
+        }
+        [HttpGet("GetBooks")]
+        [Authorize(Policy = "ManagerPolicy")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<Book>))]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        public async Task<ActionResult<List<Book>>> GetBooks()
         {
-            return View();
+            var result = await _searchService.GetBooks();
+            return result;
+        }
+
+        [HttpPost("AddBook")]
+        [Authorize(Policy = "ManagerPolicy")]
+        public async Task<IActionResult> AddBook([FromQuery] Book book)
+        {
+            try { 
+                await _searchService.AddBook(book);
+                return Ok();
+            }
+            catch (AlreadyExistsException ex)
+            {
+                throw ex;
+            }
         }
     }
 }
---
+++ BookStore/Services/BookSearchService.cs
@@ -0,0 +1,37 @@
+﻿using Infrastructure.Entities;
+using Infrastructure.Exceptions;
+using Infrastructure.Repositories;
+
+namespace BookStore.Services
+{
+    public interface IBookSearchService
+    {
+        public Task<List<Book>> GetBooks();
+        public Task AddBook(Book book);
+    }
+    public class BookSearchService : IBookSearchService
+    {
+        private readonly IBooksRepository _booksRepository;
+        private readonly IBookInfoRepository _bookInfoRepository;
+        public BookSearchService(IBooksRepository booksRepository, IBookInfoRepository bookInfoRepository) {
+            _bookInfoRepository = bookInfoRepository;
+            _booksRepository = booksRepository;
+        }
+
+        public async Task<List<Book>> GetBooks()
+        {
+            return await _booksRepository.GetBooks();
+        }
+        public async Task AddBook(Book book)
+        {
+            try
+            {
+                await _booksRepository.Create(book);
+            }
+            catch (AlreadyExistsException ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
